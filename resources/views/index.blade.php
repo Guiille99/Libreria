b19eff9003aaf095001b2bcc56ff74b97c6d216a
Arm7723 <tonachinchin@gmail.com>
2023-02-11 21:52:27 +0100

    cambios sass

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.scss'])
 </head>
 <body>
     <h1>Hola esto es el index</h1>

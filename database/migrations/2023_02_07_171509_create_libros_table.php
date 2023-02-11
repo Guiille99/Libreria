26950d2e7dfb6071f25380fa6935c2ffe2aebad6
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-02-11 20:46:05 +0100

    Commit 1

---
+++ database/migrations/2023_02_07_171509_create_libros_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string("titulo");
+            $table->string("autor", 80);
+            $table->string("editorial", 80);
+            $table->timestamp("fecha_publicacion");
+            $table->double("precio");
+            $table->string("genero", 40);
+            $table->string("descripcion");
+            $table->double("valoracion");
+            $table->integer("paginas");
+            $table->integer("stock");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+};

ea036285a46d8456a35b2314a7d530e0e272f06c
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-02-11 21:04:08 +0100

    Commit 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("username", 40);
+            $table->string("password", 40);
+            $table->string("nombre");
+            $table->string("apellidos");
+            $table->string("dni")->unique();
+            $table->string("rol", 20);
             $table->timestamps();
         });
     }

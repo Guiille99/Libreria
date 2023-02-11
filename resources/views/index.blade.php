4572d569b134d6a389ffe0df2e549276f2e3d13c
Arm7723 <tonachinchin@gmail.com>
2023-02-11 22:18:01 +0100

    cambios bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    @vite(['resources/css/app.scss'])
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <h1>Hola esto es el index</h1>

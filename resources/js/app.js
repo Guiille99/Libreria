830d5222591f9d79d94cce04ee9385cd02ee3206
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-02-17 22:45:51 +0100

    arreglo de cards

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,29 @@
 import './bootstrap'
 import '../css/app.scss'
 import * as bootstrap from 'bootstrap'
+
+addEventListener("load", inicio);
+function inicio() {
+    // $("#prueba").css("background-color", "yellow");
+    // let prueba=document.getElementById("prueba");
+    // prueba.style.backgroundColor="yellow";
+	ellipsis_box(".libro__titulo", 20);
+}
+function ellipsis_box(elemento, max_chars){
+	let titulos = $(elemento);
+    for (let i = 0; i < titulos.length; i++) {
+        if (titulos[i].innerHTML.length > max_chars) {
+            let limite = titulos[i].innerHTML.substr(0, max_chars)+" ...";
+            titulos[i].innerHTML= limite;
+        }
+        
+    }
+    // titulos.forEach(titulo => {
+    //     if (titulo.text().length > max_chars)
+    //     {
+    //     let limite = limite_text.substr(0, max_chars)+" ...";
+    //     titulo.text(limite);
+    //     }
+    // });
+}
+	

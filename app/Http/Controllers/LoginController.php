3a64eaee27ba80b8a159b025adfbb8b2ad4f5009
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-02-21 19:40:38 +0100

    modificacion loginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Dotenv\Exception\ValidationException;
 
 class LoginController extends Controller
 {
@@ -14,13 +15,14 @@ class LoginController extends Controller
     }
     public function store(Request $request){
         
+        dd($request->input("remember"));
         $credentials = $request->validate([
             "usuario" => "required",
             "password" => "required"
         ]);
 
-        if (Auth::attempt([$credentials])) { //Login success
-            
+        if (!Auth::attempt($credentials, $request->boolean("remember"))) { //Si no se loguea correctamente
+            // throw ValidationException::withMessages()
         }
     }
 }

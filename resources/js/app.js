9e16f3212af51052179159d9c2ac1203884dbf6d
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-03-09 17:26:31 +0100

    Modificacion js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,14 @@ import * as bootstrap from 'bootstrap'
 addEventListener("load", inicio);
 function inicio() {
 
-	ellipsis_box(".libro__titulo", 18);
+	// ellipsis_box(".libro__titulo", 18);
+	ellipsis_box("libro__titulo", 18);
 
 
 }
 function ellipsis_box(elemento, max_chars){
-	let titulos = $(elemento);
+	// let titulos = $(elemento);
+	let titulos = document.getElementsByClassName(elemento);
     for (let i = 0; i < titulos.length; i++) {
         if (titulos[i].innerHTML.length > max_chars) {
             let limite = titulos[i].innerHTML.substr(0, max_chars)+"...";

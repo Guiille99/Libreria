9c0a2d5d4dd624cf7018be9111b43a79c575e225
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-06-15 21:12:31 +0200

    Arreglo de incidencia leve en lectura de texto

--- resources/js/posts.js
+++ resources/js/posts.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $(".play-btn").click(toggleSpeech);
     let utterance = new SpeechSynthesisUtterance();
     let isPlaying = false;
+    let btnMicrofono = $(".play-btn");
 
     function toggleSpeech() {
-        let btnMicrofono = $(".play-btn");
         let isChecked = btnMicrofono.attr("aria-checked");
         let texto = $(".post__content-body").text();
         if (isChecked=="false") {
@@ -26,8 +26,9 @@ $(document).ready(function(){
     //Evento que se ejecuta cuando acaba de leer el texto
     utterance.onend = function(event){
         speechSynthesis.cancel();
-        $(".play-btn").removeClass("bi-pause-fill")
-        $(".play-btn").addClass("bi-volume-down-fill")
+        $(".play-btn").removeClass("bi-pause-fill");
+        $(".play-btn").addClass("bi-volume-down-fill");
+        btnMicrofono.attr("aria-checked", "false");
     }
     //Para dejear de reproducir cuando el usuario salga de la página
     $(window).on("beforeunload", function(){
